4069dd08d87f7e3dfc6cd1d8a66cd9906fd11b56
baojiwei <jiwei.bao@united-imaging.com>
2022-01-24 11:42:13 +0800

    add page component

--- amis_py/components/page.py
+++ amis_py/components/page.py
@@ -1,4 +1,14 @@
-from amis_py.components.base import Prop
+from amis_py.components.base import Prop, Properties
 
 
-class PageProperties:
+class PageProperties(Properties):
+    # do not support remark, aside, toolbar and initApi now
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        defaults = [
+            Prop("title", str, ""),
+            Prop("sub_title", str, ""),
+            Prop("class_name", str, "", "className"),
+        ]
+        self.update_defaults(defaults)
+        self.update_properties(**kwargs)
